c376dd0b3f19a38b78824f7f6ab9a71ca0139980
leticia <lds.leticia.dos.santos@gmail.com>
2020-05-20 19:33:20 -0300

    alteraçao no js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,29 @@
+/*let frutas = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
+console.log(frutas)
+alert(frutas[1].nome); pega a fruta na posição 1 e dá o nome. Esse é um dicionário de array*/
 
-let nome = "Letícia dos Santos";
-let idade = 24;
-let idade2 = 10;
-let frase = "Japão é o melhor time do mundo";
+/*let fruta = {nome: "maça", cor: "vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor); isso é um dicionário e sabemos disso porque está entre chaves*/
+
+//let lista = ["maça", "pêra", "laranja"];
+//console.log(lista.reverse()); muda a ordem da lista do último para o primeiro
+
+//console.log(lista.length); mostra o tamanho do array, nesse caso o array lista
+
+//console.log(lista.toString); deixa o array em string ao invés de elemento o que faz ele perder a ref de posição do array
+
+//console.log(lista.join(" - ")); deixa o array em string mas você pode mudar o que separa uma palavra da outra, nesse caso um -
+
+//lista.push("uva"); coloca um elemento, nesse caso a uva
+
+//lista.pop(); tira um elemento, nesse caso o último
+
+
+//let nome = "Letícia dos Santos";
+//let idade = 24;
+//let idade2 = 10;
+//let frase = "Japão é o melhor time do mundo";
 //alert(nome + " tem " + idade);
 //alert(idade + idade2);
-alert(frase.replace("Japão é", "Brasil era")); //replace troca a informação da variável que tem string sem ter que mexer na declaração da var
+//alert(frase.replace("Japão é", "Brasil era")); //replace troca a informação da variável que tem string sem ter que mexer na declaração da var
