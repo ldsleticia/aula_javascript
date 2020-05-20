b95f08d113eaabe56a3185c7b6f8d8ca81954bfc
leticia <lds.leticia.dos.santos@gmail.com>
2020-05-20 18:52:14 -0300

    pasta js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+let nome = "Letícia dos Santos";
+let idade = 24;
+let idade2 = 10;
+let frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade);
+//alert(idade + idade2);
+alert(frase.replace("Japão é", "Brasil era")); //replace troca a informação da variável que tem string sem ter que mexer na declaração da var
