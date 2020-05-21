cede021584b49ce372351a98d62361a87bc6cdb0
leticia <lds.leticia.dos.santos@gmail.com>
2020-05-20 21:21:08 -0300

    alteraçao no js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,50 @@
+/*function soma(n1, n2){
+    return n1 + n2;
+} //função de soma
+alert(soma(5,10));*/
+
+/*function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome);
+}
+alert(setReplace("Vai Japão", "Japão", "Brasil")) aqui está setada a frase, o que é pra mudar da frase e o que é pra colocar no lugar com o replace*/
+
+function validaIdade(idade){
+    let validar;
+    if(idade >= 18){
+        validar = true
+    } else{
+        validar = false
+    }
+    return validar; //ele precisa retornar validar porque é o validar que faz toda a função rodar e vai te dar se a idade é true ou false
+
+}
+
+let idade = parseInt(prompt("digite sua idade"));
+console.log(validaIdade(idade));
+
+
+
+/*let d = new Date(); quando vazio mostra a data completa do dia
+alert(d.getMonth()+1); com getMonth, ele vai mostrar só o mês começando do 0, sempre será necessário colocar o +1
+//alert(d.getDay()); pega só o dia
+//alert(d.getMinutes()); pega só os minutos
+/*for (let contador = 0; contador <= 100; contador++){
+    console.log(contador);
+}/*
+
+/*let contador = 0;
+while (contador <= 100){
+    console.log(contador);
+    contador++;
+} colocar o console.log antes do final do laço para ficar a conta certa/*
+
+/*let idade = parseInt(prompt("digite sua idade"));
+if(idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+} ensino do if,else*/
+
 /*let frutas = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
 console.log(frutas)
 alert(frutas[1].nome); pega a fruta na posição 1 e dá o nome. Esse é um dicionário de array*/
